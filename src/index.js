580806d9b491e97b84d27b7300b5fc6b6460c51f
Nasidul Islam <nasidulislam@gmail.com>
2018-09-30 18:27:48 -0400

    inital-commit: basic app layout setup.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
 
 //components
-import StorePicker from './components/StorePicker';
+// import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-
-
-render(<StorePicker/>, document.querySelector('#main'));
+// render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
