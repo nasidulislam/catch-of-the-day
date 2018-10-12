3eec353a2c16b26a1564d9eef345eb701fdf781b
Nasidul Islam <nasidulislam@gmail.com>
2018-10-11 20:18:37 -0400

    events: attach event handler and handle route changes.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+	goToStore = (event) => {
+		const storeName = this.myInput.value.value;
+
+		event.preventDefault();
+		this.props.history.push(`/store/${storeName}`);
+	};
+
+	myInput = React.createRef();
+
 	render() {
 		return (
-			<form className='store-selector'>
+			<form className='store-selector' onSubmit={this.goToStore}>
 				<h2>Please Enter a Store</h2>
-				<input type='text' required placeholder='Store Name'/>
+				<input
+					type='text'
+					required
+					placeholder='Store Name'
+					ref={this.myInput}
+					defaultValue={getFunName()}/>
 				<button type='submit'>Visit Store</button>
 			</form>
 		)
